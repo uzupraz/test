2230a4ae5c16327c2fc8d03ab298813e8bbf1268
Aanal Raj Basaula <a.basaula@elephanti-soft.de>
2023-12-27 19:25:10 +0100

    Adds endpoint to list all templates

---
+++ service/processor_template_service.py
@@ -0,0 +1,29 @@
+from typing import List
+
+from model import ProcessorTemplate
+from repository import ProcessorTemplateRepo
+from controller import common_controller as common_ctrl
+from utils import Singleton
+
+
+log = common_ctrl.log
+
+
+class ProcessorTemplateService(Singleton):
+
+
+    _instance = None
+
+
+    def __init__(self, repo: ProcessorTemplateRepo) -> None:
+        self.repo = repo
+
+
+    def get_all_templates(self) -> List[ProcessorTemplate]:
+        """
+        List all the Processor templates available.
+
+        Returns:
+            A list of processor templates, empty list if not available
+        """
+        return self.repo.get_all_templates()
