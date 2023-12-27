17f50860a9c762608a881fad3586c365bd94196f
Aanal Raj Basaula <a.basaula@elephanti-soft.de>
2023-12-27 12:24:49 +0100

    Adds Processor Template repo

--- tests/repository/test_processor_template_repo.py
+++ tests/repository/test_processor_template_repo.py
@@ -29,7 +29,7 @@ class TestProcessTemplateRepo(unittest.TestCase):
             self.repo.table = self.mock_table
 
 
-    def test_get_all_templates_success(self):
+    def test_get_all_templates_success_should_return_items(self):
         # Mock response from DynamoDB
         items = [TestUtils.get_file_content(self.test_resource_path + 'event_processor_template.json')]
         self.mock_table.scan.return_value = {'Items': items}
@@ -48,7 +48,7 @@ class TestProcessTemplateRepo(unittest.TestCase):
         self.mock_table.scan.assert_called_once()
 
 
-    def test_get_all_templates_empty(self):
+    def test_get_all_templates_empty_database_should_return_empty_list(self):
         # Mock empty response from DynamoDB
         self.mock_table.scan.return_value = {'Items': []}
 
@@ -57,3 +57,15 @@ class TestProcessTemplateRepo(unittest.TestCase):
 
         # Assertions
         self.assertEqual(len(templates), 0)
+
+
+    def test_get_all_templates_dynamodb_exception_should_raise_service_exception(self):
+        # Mock a DynamoDB exception
+        self.mock_table.scan.side_effect = ClientError({'Error': {'Message': 'Test Error'}, 'ResponseMetadata': {'HTTPStatusCode': 400}}, 'scan')
+
+        # Test exception handling
+        with self.assertRaises(ServiceException) as context:
+            self.repo.get_all_templates()
+
+        # Assertions
+        self.assertEqual('Could not load available templates list', context.exception.message)
