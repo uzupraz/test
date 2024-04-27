4988b51d5be29e8a42e1f6a16654bdb50a4c267b
Aanal Raj Basaula <a.basaula@elephanti-soft.de>
2024-04-27 14:35:07 +0200

    Adds endponits for file processing

--- controller/files_resource.py
+++ controller/files_resource.py
@@ -3,22 +3,21 @@ from flask import request
 
 from .server_response import ServerResponse
 from .common_controller import server_response
-from configuration import AWSConfig, AppConfig
-from service import ProcessorTemplateService
-from repository import ProcessorTemplateRepo
 from enums import APIStatus
+from service import S3FileService
+from configuration import AsyncFileDeliveryS3Config
 
 
-api = Namespace('Files API', description='Manages operations related to files.', path='/files')
+api = Namespace('Files API', description='Manages operations related to async submission of files.', path='/files')
 log = api.logger
 
 file_dto = api.model('Processor Template', {
-    'filename': fields.String(required=True),
+    'path': fields.String(required=True),
     'url': fields.String(required=True)
 })
 
-list_files_dto = api.inherit('Listed Files', server_response, {
-       'payload': fields.List(fields.Nested(file_dto))
+response_list_files_dto = api.inherit('Listed Files', server_response, {
+    'payload': fields.List(fields.Nested(file_dto))
 })
 
 request_file_upload_dto = api.model('Request File Upload', {
@@ -26,10 +25,12 @@ request_file_upload_dto = api.model('Request File Upload', {
     'path': fields.String(required=True)
 })
 
-request_file_upload_response_dto = api.inherit('Pre Signed URL Response Model', server_response, {
-        'payload': fields.Nested(api.model('URL Dictionary', {
-        'url': fields.String
-    }))
+upload_file_dto = api.model('Approved File Upload', {
+    'url': fields.String
+})
+
+response_file_upload_dto = api.inherit('Pre Signed URL Response Model', server_response, {
+        'payload': fields.Nested(upload_file_dto)
 })
 
 @api.route('')
@@ -38,25 +39,51 @@ class FilesResource(Resource):
 
     def __init__(self, api=None, *args, **kwargs):
         super().__init__(api, *args, **kwargs)
+        self.file_delivery_config = AsyncFileDeliveryS3Config()
+        self.s3_file_service = S3FileService(self.file_delivery_config)
 
 
-    @api.doc(description='List all files in a defined folder', params={'path': {'in': 'query', 'description': 'The path for which the files should be listed.', 'type': 'string', 'required': True}})
-    @api.marshal_with(list_files_dto, skip_none=True)
+    @api.doc(description='List all files in a defined folder', params={
+            'path': {'in': 'query', 'description': 'The path for which the files should be listed.', 'type': 'string', 'required': True},
+            'owner': {'in': 'query', 'description': 'The owner id for which the files should be listed', 'type': 'string', 'required': True}
+        })
+    @api.marshal_with(response_list_files_dto, skip_none=True)
     def get(self):
         log.info('Received API Request. api: %s, method: %s, status: %s', request.url, request.method, APIStatus.START)
+        owner_id = request.args.get('owner')
+        relative_path = request.args.get('path')
+        result = self.s3_file_service.list_files_in_output_folder(owner_id, relative_path)
         log.info('Done API Invocation. api: %s, method: %s, status: %s', request.url, request.method, APIStatus.SUCCESS)
-        return ServerResponse.success(payload=[]), 200
+        return ServerResponse.success(payload=result), 200
     
     
     @api.doc(description='Gets the pre-signed url from S3 for async file delivery')
     @api.expect(request_file_upload_dto, validate=True)
-    @api.marshal_with(request_file_upload_response_dto, skip_none=True)
+    @api.marshal_with(response_file_upload_dto, skip_none=True)
     def post(self):
-        return ServerResponse.success(payload=None), 200
+        log.info('Received API Request. api: %s, method: %s, status: %s', request.url, request.method, APIStatus.START)
+        request_body = api.payload
+        pre_signed_url = self.s3_file_service.generate_upload_pre_signed_url(request_body['owner_id'], request_body['path'])
+        log.info('Done API Invocation. api: %s, method: %s, status: %s', request.url, request.method, APIStatus.SUCCESS)
+        return ServerResponse.success(payload={"url": pre_signed_url}), 200
     
 
+@api.route('/confirm-download')
+class ConfirmDownloadResource(Resource):
+
+
+    def __init__(self, api=None, *args, **kwargs):
+        super().__init__(api, *args, **kwargs)
+        self.file_delivery_config = AsyncFileDeliveryS3Config()
+        self.s3_file_service = S3FileService(self.file_delivery_config)
+
+    
     @api.doc(description='Update file state')
     @api.expect(request_file_upload_dto, validate=True)
     @api.marshal_with(server_response, skip_none=True)
-    def put(self):
+    def post(self):
+        log.info('Received API Request. api: %s, method: %s, status: %s', request.url, request.method, APIStatus.START)
+        request_body = api.payload
+        self.s3_file_service.archive_output_file(request_body['owner_id'], request_body['path'])
+        log.info('Done API Invocation. api: %s, method: %s, status: %s', request.url, request.method, APIStatus.SUCCESS)
         return ServerResponse.success(payload=None), 200
--- service/s3_file_service.py
+++ service/s3_file_service.py
@@ -1,6 +1,7 @@
 import boto3
 
 from botocore.exceptions import ClientError
+from typing import List
 
 from controller import common_controller as common_ctrl
 from utils import Singleton
@@ -18,26 +19,128 @@ class S3FileService(metaclass=Singleton):
     def __init__(self, file_delivery_config: AsyncFileDeliveryS3Config) -> None:
         self.s3_client = boto3.client('s3')
         self.file_delivery_config = file_delivery_config
+        print(self.file_delivery_config)
 
-
-    def get_pre_signed_url(self, owner_id: str, file_name: str) -> str:
+    
+    def _generate_s3_key(self, owner_id:str, relative_path:str) -> str:
         """
-        Gets a pre-signed url to upload a file to S3.
-
+        Generates an s3 key for the provided details.
+        Args:
+            owner_id (str): The owner for which the data should be gathered
+            relative_path (str): The relative for the owner from its home directory including the filename
         Returns:
-            str: Pre-signed URL
+            str: S3 Key
         """
-        log.info('Getting pre-signed url. owner_id: %s, file_name: %s', owner_id, file_name)
+        s3_key = ''
+        relative_path = relative_path.lstrip('/')
+        if self.file_delivery_config.object_prefix:
+            s3_key = f'{self.file_delivery_config.object_prefix}/{owner_id}/{relative_path}'
+        else:
+            s3_key = f'{owner_id}/{relative_path}'
+        return s3_key
+    
+
+    def _generate_pre_signed_url(self, bucket_id:str, s3_key:str, action:str) -> str:
+        """
+        Generates a S3 presigned URL based on the provided information.
+        Args:
+            bucket_id(str): The bucket for which the presigned URL is to be generated
+            s3_key(str): The S3 object key
+            action(str): The action the url should allow. examples: put_object, get_object
+        """
+        log.info('Getting pre-signed url. bucket_id: %s, s3_key: %s, action: %s', bucket_id, s3_key, action)
         try:
             url = self.s3_client.generate_presigned_url(
-                'put_object',
+                action,
                 Params={
-                    'Bucket': self.file_delivery_config.input_bucket_name,
-                    'Key': f'{self.file_delivery_config.object_prefix}/{owner_id}/{file_name}'
+                    'Bucket': bucket_id,
+                    'Key': s3_key
                 },
                 ExpiresIn=self.file_delivery_config.pre_signed_url_expiration
             )
             return url
         except ClientError as e:
-            log.exception('Failed to get pre-signed url. owner_id: %s, file_name: %s', owner_id, file_name)
+            log.exception('Failed to get pre-signed url. bucket_id: %s, s3_key: %s, action: %s', bucket_id, s3_key, action)
             raise ServiceException(e.response['ResponseMetadata']['HTTPStatusCode'], ServiceStatus.FAILURE, 'Could not get pre-signed url')
+    
+
+    def list_files_in_output_folder(self, owner_id:str, relative_path:str) -> List[any]:
+        """
+        Lists files available in the output bucket. The folder is defined in the relative path from the home folder of the owner.
+        Args:
+            owner_id (str): The id of the owner for which the files is to be listed
+            relative_path (str): The relative path of the folder from the home directory where the files are to be listed
+        Returns:
+            List[dict]: A list of dictionary with path and url
+        """
+        s3_key = self._generate_s3_key(owner_id, relative_path)
+        response = self.s3_client.list_objects_v2(
+            Bucket=self.file_delivery_config.output_bucket_name,
+            Prefix=s3_key
+        )
+        results = []
+
+        for obj in response.get('Contents', []):
+            results.append({
+                'path': obj['Key'],
+                'url': self._generate_pre_signed_url(self.file_delivery_config.output_bucket_name, obj['Key'], 'get_object')
+            })
+
+        return results
+    
+
+    def move_file(self, source_bucket:str, source_key:str, dest_bucket:str, dest_key:str):
+        """
+        Moves a file from the defined source bucket to the defined destination bucket. Note that the buckets could also be the same.
+        Args:
+            source_bucket (str): The source bucket id
+            source_key (str): Source Object key
+            dest_bucket (str): Destination bucket id. Either different or the same
+            dest_key (str): Destination object key
+        """
+        log.info('Moving S3 file. src_bucket: %s, src_key: %s, dest_bucket: %s, dest_key: %s', source_bucket, source_key, dest_bucket, dest_key)
+        assert not (source_bucket == dest_bucket and source_key == dest_key ), 'Cannot move from same location to the same location (bucket + key)'
+        try:
+            self.s3_client.copy_object(Bucket=dest_bucket, Key=dest_key, CopySource={
+                'Bucket': source_bucket,
+                'Key': source_key
+            })
+        except ClientError as e:
+            log.info('Failed to copy S3 file. src_bucket: %s, src_key: %s, dest_bucket: %s, dest_key: %s', source_bucket, source_key, dest_bucket, dest_key)
+            raise ServiceException(e.response['ResponseMetadata']['HTTPStatusCode'], ServiceStatus.FAILURE, 'Could not move S3 File')
+        log.info('Successfully copied file to destination bucket. src_bucket: %s, src_key: %s, dest_bucket: %s, dest_key: %s', source_bucket, source_key, dest_bucket, dest_key)
+
+        try:
+            self.s3_client.delete_object(Bucket=source_bucket, Key=source_key)
+        except ClientError as e:
+            log.info('Failed to delete S3 file after copy. src_bucket: %s, src_key: %s, dest_bucket: %s, dest_key: %s', source_bucket, source_key, dest_bucket, dest_key)
+            raise ServiceException(e.response['ResponseMetadata']['HTTPStatusCode'], ServiceStatus.FAILURE, 'Could not move S3 File')
+        log.info('Successfully deleted source file. src_bucket: %s, src_key: %s, dest_bucket: %s, dest_key: %s', source_bucket, source_key, dest_bucket, dest_key)
+
+
+    def archive_output_file(self, owner_id:str, relative_path:str):
+        """
+        Archives the defined output file.
+        Args:
+            owner_id (str): The Owner Id who owns the file
+            relative_path (str): The relative path to the file from the owner's home foldwer
+        """
+        log.info('Archiving output file. owner_id: %s, relative_path: %s', owner_id, relative_path)
+        s3_key = self._generate_s3_key(owner_id, relative_path)
+        self.move_file(self.file_delivery_config.output_bucket_name, s3_key, self.file_delivery_config.archive_bucket_name, s3_key)
+        log.info('Successfully archived output file. owner_id: %s, relative_path: %s', owner_id, relative_path)
+
+
+    def generate_upload_pre_signed_url(self, owner_id:str, relative_path:str) -> str:
+        """
+        Gets a pre-signed url to upload a file to S3 Input Bucket.
+        Args:
+            owner_id (str): The owner for which the data should be gathered
+            relative_path (str): The relative for the owner from its home directory including the filename
+        Returns:
+            str: Pre-signed URL
+        """
+        s3_key = self._generate_s3_key(owner_id, relative_path)
+        log.info('Getting pre-signed url. owner_id: %s, relative_path: %s', owner_id, relative_path)
+        return self._generate_pre_signed_url(self.file_delivery_config.input_bucket_name, s3_key, 'put_object')
+    
