442604f74f3822abfc0a7d6362212bac0488ddee
Ujwal Parajuli <ujwal.aerialarts@gmail.com>
2024-06-21 10:35:27 +0545

    Adds log for debugging

--- context/request_context.py
+++ context/request_context.py
@@ -48,6 +48,7 @@ class RequestContext:
         Args:
             event: The event object containing the token validation info.
         """
+        print("event: " + event)
         claims = event['requestContext']['authorizer']['claims']
         user = User.from_authorizer_claims(claims)
         g.user = asdict(user)
