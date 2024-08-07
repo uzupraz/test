e36307b80c92181b420106bcda3f0f1073bcd5b4
narayan.aerialarts <narayan.aerialarts@gmail.com>
2024-08-07 15:34:45 +0545

    resolve return type of data-table repo

--- repository/customer_table_repository.py
+++ repository/customer_table_repository.py
@@ -31,7 +31,7 @@ class CustomerTableRepository(metaclass=Singleton):
         self.dynamodb_resource = self.__configure_dynamodb_resource()
 
 
-    def get_table_items(self, table_name:str, limit:int, exclusive_start_key:dict=None) -> tuple[list, str|None]:
+    def get_table_items(self, table_name:str, limit:int, exclusive_start_key:dict=None) -> tuple[list, dict|None]:
         """
         Retrieve items from a DynamoDB table with pagination.
 
